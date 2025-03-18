1194e071449a28361ac826f364eb714be8333c67
Anurag1111111 <anujangir1707@gmail.com>
2025-03-18 15:12:16 +0530

    Firebase_options added

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,14 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 import 'theme_provider.dart';
 import 'todo_screen.dart';
+import 'firebase_options.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(
     ChangeNotifierProvider(
